8e63dde5b78ed8301c3106c92716d72ba244f018
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-06-27 20:23:23 +0300

    Добавлены функция открытия/закрытия popup Login

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let link = document.querySelector('.login-link'),
+  popup = document.querySelector('.modal-login'),
+  close = document.querySelector('.modal-close');
+
+link.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('modal-show')
+});
+
+close.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('modal-show')
+});
