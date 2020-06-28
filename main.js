02a409ae8554cdb6e0d11a57c9fd23f7fa638550
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-06-28 09:20:37 +0300

    Добавлены функции проверки ввода данных popup Login

--- main.js
+++ main.js
@@ -2,14 +2,43 @@
 
 let link = document.querySelector('.login-link'),
   popup = document.querySelector('.modal-login'),
-  close = document.querySelector('.modal-close');
+  close = document.querySelector('.modal-close'),
+  form = popup.querySelector('form'),
+  login = popup.querySelector('[name=login]'),
+  password = popup.querySelector('[name=password]'),
+  isStorageSupport = true,
+  storage = "";
+
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.add('modal-show')
+  login.focus();
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
 });
 
 close.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('modal-show')
+});
+
+form.addEventListener('submit', function (evt) {
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    alert('Нужно ввести пароль!')
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value)
+    }
+  }
 });
