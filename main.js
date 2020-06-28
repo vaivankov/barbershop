7be75c5e2dd2500e345c01325ff23dfde4e8f1ba
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-06-28 09:42:12 +0300

    Добавлено закрытие popup Login по клавише ESC

--- main.js
+++ main.js
@@ -42,3 +42,12 @@ form.addEventListener('submit', function (evt) {
     }
   }
 });
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27){
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show")
+    }
+  }
+})
