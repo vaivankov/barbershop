b193253571ffa755dd9dd2a95e709e05a67c3200
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-06-28 11:16:12 +0300

    Повышена безопасность JS кода

--- main.js
+++ main.js
@@ -1,56 +1,73 @@
 'use strict';
 
-let link = document.querySelector('.login-link'),
-  popup = document.querySelector('.modal-login'),
-  close = document.querySelector('.modal-close'),
-  form = popup.querySelector('form'),
-  login = popup.querySelector('[name=login]'),
-  password = popup.querySelector('[name=password]'),
-  isStorageSupport = true,
-  storage = "";
+window.loginLink = document.querySelector('.login-link');
+window.loginPopup = document.querySelector('.modal-login');
+window.loginPopupClose = document.querySelector('.modal-close');
+window.loginPopupForm = loginPopup.querySelector('form');
+window.loginPopupLogin = loginPopup.querySelector('[name=login]');
+window.loginPopupPassword = loginPopup.querySelector('[name=password]');
+window.isStorageSupport = true;
+window.storage = "";
 
 try {
-  storage = localStorage.getItem('login');
+  window.storage = localStorage.getItem('login');
 } catch (err) {
-  isStorageSupport = false;
+  window.isStorageSupport = false;
 }
 
-link.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.add('modal-show')
-  
-  if (storage) {
-    login.value = storage;
-    password.focus();
-  } else {
-    login.focus();
-  }
-});
-
-close.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  popup.classList.remove('modal-show');
-  popup.classList.remove('modal-error');
-});
-
-form.addEventListener('submit', function (evt) {
-  if (!login.value || !password.value) {
+(function () {
+  window.showModalWidow = function (evt) {
     evt.preventDefault();
-    popup.classList.remove('modal-error');
-    popup.scrollLeft = popup.scrollLeft;
-    popup.classList.add('modal-error');
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("login", login.value)
-    }
-  }
-});
+    window.loginPopup.classList.add('modal-show')
 
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27){
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show")
-      popup.classList.remove('modal-error');
+    if (storage) {
+      window.loginPopupLogin.value = window.storage;
+      window.loginPopupPassword.focus();
+    } else {
+      window.loginPopupLogin.focus();
     }
   }
-})
+})();
+
+(function () {
+  window.closeModalWindow = function (evt) {
+    evt.preventDefault();
+    window.loginPopup.classList.remove('modal-show');
+    window.loginPopup.classList.remove('modal-error');
+  }
+})();
+
+(function () {
+  window.validateForm = function (evt) {
+    if (!window.loginPopupLogin.value || !window.loginPopupPassword.value) {
+      evt.preventDefault();
+      window.loginPopup.classList.remove('modal-error');
+      window.loginPopup.scrollLeft = loginPopup.scrollLeft;
+      window.loginPopup.classList.add('modal-error');
+    } else {
+      if (window.isStorageSupport) {
+        window.localStorage.setItem("login", window.loginPopupLogin.value)
+      }
+    }
+  }
+})();
+
+(function () {
+  window.checkIsHotKey = function (evt) {
+    if (evt.keyCode === 27) {
+      let modal;
+      try {
+        modal = document.querySelector(".modal-show");
+      } catch (err) {};
+      if (modal) {
+        modal.classList.remove("modal-show")
+        modal.classList.remove('modal-error');
+      };
+    }
+  }
+})();
+
+window.loginLink.addEventListener('click', showModalWidow);
+window.loginPopupClose.addEventListener('click', closeModalWindow);
+window.loginPopupForm.addEventListener('submit', validateForm);
+window.addEventListener('keydown', checkIsHotKey);
