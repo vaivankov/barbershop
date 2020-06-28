f1bb6371f73f92d95dac251db35401caed6de4bb
vaivankov <ivankov.vitalii.2017@yandex.ru>
2020-06-28 14:15:57 +0300

    Добавлена функции установки текущей даты

---
+++ js/func__setTodaysDate.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function () {
+  window.dateNow = new Date();
+  window.dateFull = dateNow.toISOString();
+  window.dateShort = dateFull.match(/\d{4}-\d{2}-\d{2}/);
+  document.querySelector('input[type="date"]').value = dateShort;
+})()
